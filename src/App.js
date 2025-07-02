2d032ed116049ee2f688c076b95423019f914b7b
Hannah <hannahqi314@gmail.com>
2025-07-01 20:28:44 -0400

    deploying website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
 import AboutMe from './pages/AboutMe';
