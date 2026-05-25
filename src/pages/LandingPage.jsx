c6d9e703c0da6f94787355a0f3a8770c7b425d75
Hannah Qi <hannahqi314@gmail.com>
2026-05-25 18:35:57 -0400

    changed font and title for landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,11 +7,16 @@ import lightClouds from '../../assets/Light clouds.png';
 function LandingPage() {
   return (
     <main className="landing-page">
-      <Link to="/menu" aria-label="Enter portfolio">
-        <div className="landing-copy">
-          <h1>Hannah Qi</h1>
-        </div>
-      </Link>
+      <div className="landing-copy">
+        <p className="landing-kicker">Portfolio</p>
+        <Link to="/menu" className="landing-title" aria-label="Enter Hannah Qi's portfolio">
+          <h1>
+            Hannah
+            <span>Qi</span>
+          </h1>
+        </Link>
+        <p className="landing-invitation">Click my name to explore</p>
+      </div>
 
       <div className="landing-visual">
         <div className="landing-stack" aria-hidden="true">
@@ -19,7 +24,6 @@ function LandingPage() {
           <img src={lightCircle} alt="Light circle" className="layer circle" />
           <img src={clouds2} alt="Cloud detail" className="layer clouds-2" />
           <img src={lightClouds} alt="Light cloud detail" className="layer light-clouds" />
-          <div className="landing-click-hint">click on my name to enter my portfolio!</div>
         </div>
       </div>
     </main>
